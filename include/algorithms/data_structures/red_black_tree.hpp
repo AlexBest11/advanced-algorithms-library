c78e99cb8ebb904f5d13b0b99b2cfbaa7c3c0201
MaxKozlovITMOStudent <zavolnie@gmail.com>
2026-08-19 17:34:17 +0300

    we add delete red_black_tree

--- include/algorithms/data_structures/red_black_tree.hpp
+++ include/algorithms/data_structures/red_black_tree.hpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <iostream>
+#include <utility>
 #include <vector>
 
 enum class Color
@@ -142,6 +143,122 @@ class RB_Tree
 		}
 	}
 
+	Node* find_min_right_side(Node* n)
+	{
+		Node* current = n->right;
+		if (current == nullptr)
+		{
+			return n;
+		}
+		while (current->left != nullptr)
+		{
+			current = current->left;
+		}
+		return current;
+	}
+	Color get_color(Node* n) { return (n == nullptr) ? Color::black : n->color; }
+
+	void fix_delete(Node* node, Node* parent)
+	{
+		while (node != root && parent != nullptr && get_color(node) == Color::black)
+		{
+			if (node == parent->left)
+			{
+				Node* brother = parent->right;
+				if (get_color(brother) == Color::red)
+				{
+					brother->color = Color::black;
+					parent->color = Color::red;
+					left_rotate(parent);
+					brother = parent->right;
+				}
+				Color brother_left_color = (brother != nullptr) ? get_color(brother->left) : Color::black;
+				Color brother_right_color = (brother != nullptr) ? get_color(brother->right) : Color::black;
+
+				if (brother_left_color == Color::black && brother_right_color == Color::black)
+				{
+					if (brother != nullptr)
+					{
+						brother->color = Color::red;
+					}
+					node = parent;
+					parent = node->parent;
+				}
+				else
+				{
+					if (brother_right_color == Color::black)
+					{
+						if (brother->left != nullptr)
+						{
+							brother->left->color = Color::black;
+						}
+						brother->color = Color::red;
+						right_rotate(brother);
+						brother = parent->right;
+					}
+					brother->color = parent->color;
+					parent->color = Color::black;
+					if (brother->right != nullptr)
+					{
+						brother->right->color = Color::black;
+					}
+					left_rotate(parent);
+					node = root;
+				}
+			}
+			else
+			{
+				Node* brother = parent->left;
+				if (get_color(brother) == Color::red)
+				{
+					brother->color = Color::black;
+					parent->color = Color::red;
+					right_rotate(parent);
+					brother = parent->left;
+				}
+
+				Color brother_left_color = (brother != nullptr) ? get_color(brother->left) : Color::black;
+				Color brother_right_color = (brother != nullptr) ? get_color(brother->right) : Color::black;
+
+				if (brother_left_color == Color::black && brother_right_color == Color::black)
+				{
+					if (brother != nullptr)
+					{
+						brother->color = Color::red;
+					}
+					node = parent;
+					parent = node->parent;
+				}
+				else
+				{
+					if (brother_left_color == Color::black)
+					{
+						if (brother->right != nullptr)
+						{
+							brother->right->color = Color::black;
+						}
+						brother->color = Color::red;
+						left_rotate(brother);
+						brother = parent->left;
+					}
+					brother->color = parent->color;
+					parent->color = Color::black;
+					if (brother->left != nullptr)
+					{
+						brother->left->color = Color::black;
+					}
+					right_rotate(parent);
+					node = root;
+				}
+			}
+		}
+
+		if (node != nullptr)
+		{
+			node->color = Color::black;
+		}
+	}
+
   public:
 	RB_Tree() = default;
 	RB_Tree(const RB_Tree&) = delete;
@@ -193,6 +310,86 @@ class RB_Tree
 			else
 			{
 				return;
+			}
+		}
+	}
+
+	Node* find(const T& value)
+	{
+		Node* current = root;
+		while (current != nullptr)
+		{
+			if (current->data < value)
+			{
+				current = current->right;
+			}
+			else if (current->data > value)
+			{
+				current = current->left;
+			}
+			else
+			{
+				return current;
+			}
+		}
+		return nullptr;
+	}
+
+	void remove(const T& value)
+	{
+		Node* n = find(value);
+		if (n != nullptr)
+		{
+			if (n->left != nullptr && n->right != nullptr)
+			{
+				Node* m = find_min_right_side(n);
+				std::swap(n->data, m->data);
+				n = m;
+			}
+			Node* child = nullptr;
+			if (n->left != nullptr)
+			{
+				child = n->left;
+			}
+			if (n->right != nullptr)
+			{
+				child = n->right;
+			}
+			Node* parent = n->parent;
+			Color removed_color = n->color;
+			if (child != nullptr)
+			{
+				child->parent = parent;
+			}
+			if (parent == nullptr)
+			{
+				root = child;
+			}
+			else if (parent->left == n)
+			{
+				parent->left = child;
+			}
+			else
+			{
+				parent->right = child;
+			}
+			delete n;
+			size -= 1;
+
+			if (removed_color == Color::red)
+			{
+				return;
+			}
+			else
+			{
+				if (child != nullptr && child->color == Color::red)
+				{
+					child->color = Color::black;
+				}
+				else
+				{
+					fix_delete(child, parent);
+				}
 			}
 		}
 	}
